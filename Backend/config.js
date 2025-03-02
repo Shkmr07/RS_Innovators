a50f975bdd7038287838d85b61c2283dbdd7e9e0
Ayush Kumar Singh <shkmrsingh@gmail.com>
2025-03-02 17:37:33 +0530

    connect to mongodB altas

--- Backend/config.js
+++ Backend/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/Gamify_Wellness");
+    await mongoose.connect(`mongodb+srv://shkmr07:${process.env.DB}@gamifywellness.j53tk.mongodb.net/Gamify_wellness`);
     console.log("✅ Connected to Db.");
   } catch (err) {
     console.log("❌ Error connecting to mongoDB.");
